0c94a7106056d3fe093b65ea52a16b2ca5d8d9a2
Rishab Patwari <patwariri@gmail.com>
2020-02-15 20:12:50 -0800

    Added flatList

--- insureMe/components/Insurance/InsuranceCards.js
+++ insureMe/components/Insurance/InsuranceCards.js
@@ -1,18 +1,41 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View, Button, useWindowDimensions, Dimensions } from 'react-native';
+import { Platform, StyleSheet, Text, FlatList, View, SafeAreaView } from 'react-native';
 import { Component } from "react";
-    //Name
 
-    //5 images
 
-    //Description
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ];
+
+function Item({title}) {
+    return (
+        <View style={styles.listItem}>
+            <Text style={styles.title}>{title}</Text>
+        </View>
+    )
+}
+
 export default class InsuranceCard extends Component {
     render() {
         return (
-            <View style = {styles.container}>
-                <View style={{backgroundColor: 'blue', flex: 0.3}} />
-                <View style={{backgroundColor: 'red', flex:0.3}} />
-            </View>
+            <SafeAreaView style = {styles.container}>
+                <FlatList
+                data={DATA}
+                renderItem={({ item }) => <Item title={item.title} />}
+                keyExtractor={item => item.id}>
+                </FlatList>
+            </SafeAreaView>
         )
     }
 }
@@ -20,6 +43,7 @@ export default class InsuranceCard extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: 'row',
         backgroundColor: 'black',
         borderColor: "#0000",
         borderRadius: 50,
@@ -27,5 +51,14 @@ const styles = StyleSheet.create({
     },
     item: {
         flex: 0.3,
-    }
+    },
+    listItem: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+      },
+      title: {
+        fontSize: 32,
+      },
 })
