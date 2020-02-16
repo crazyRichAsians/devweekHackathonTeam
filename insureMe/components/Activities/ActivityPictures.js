7bccb3206544a048a48ebc61dd38378f3defb653
James Chua <jameschua@Jamess-MacBook-Air.local>
2020-02-16 09:42:27 -0800

    more cards for activities

--- insureMe/components/Activities/ActivityPictures.js
+++ insureMe/components/Activities/ActivityPictures.js
@@ -1,5 +1,20 @@
 export const Pics = [
   {
+    pic: require('../../assets/images/activities/sky-dive.jpg'),
+    title: 'Skydive California',
+    caption: '150 miles away',
+  },
+  {
+    pic: require('../../assets/images/activities/rock-climbing.jpg'),
+    title: 'Rock Climbing at Joshua Tree National Park',
+    caption: '600 miles away',
+  },
+  {
+    pic: require('../../assets/images/activities/jetski.jpg'),
+    title: 'Jet Ski San Diego',
+    caption: '500 miles away',
+  },
+  {
     pic: require('../../assets/images/activities/alcatraz.jpg'),
     title: 'Alcatraz',
     caption: '16 miles away',
